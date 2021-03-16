70272e19352ddb6ba101affbf28a58bb2d59ef6e
Rugie-Ann Barrameda <rbarrameda@NISGAA.BC.CA>
2021-03-16 15:20:42 -0700

    First commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/cms/dashboard', [DashboardController::class, 'index']);
+Route::get('/signin', [AuthController::class, 'signin']);
+Route::get('/callback', [AuthController::class, 'callback']);
+Route::get('/signout', [AuthController::class, 'signout']);
